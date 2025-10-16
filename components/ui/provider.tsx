2f1737b84a32b6edd73388c314861876cc34811d
Zsolt Kerekes <kerekes.zsolt.job@gmail.com>
2025-10-16 21:37:38 +0200

    adding type check as well

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { ColorModeProvider } from "./color-mode";
+import type { ThemeProviderProps } from "next-themes";
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider(props: ThemeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
