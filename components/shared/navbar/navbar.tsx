8f0ac8d691f726ce5e11997cc2a25bd7f2c9c956
Zsolt Kerekes <kerekes.zsolt.job@gmail.com>
2025-10-17 18:07:01 +0200

    Added logo, and the proper app name

--- components/shared/navbar/navbar.tsx
+++ components/shared/navbar/navbar.tsx
@@ -1,12 +1,19 @@
 import { ColorModeButton } from "@/components/ui/color-mode";
 import styles from "./navbar.module.css";
 import { Heading, Link } from "@chakra-ui/react";
+import Image from "next/image";
 
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <Link href="/" marginRight="auto">
-        <Heading as={"h1"}>Bookshelf</Heading>
+        <Image
+          src="/bookshelf.svg"
+          alt="Bookshelf logo"
+          height={20}
+          width={20}
+        />
+        <Heading as={"h1"}>Bookshelf at Home</Heading>
       </Link>
       <ColorModeButton />
     </nav>
